e4f5f4d4fb8cd243c2bdf7b07c11d7969651dfbd
Amr Fekry <amrfekryy@gmail.com>
2021-09-28 16:23:28 +0200

    fix CORS

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -3,7 +3,12 @@ const items = require('../assets/data')
 exports.handler = async (event, context, cb) => {
     return {
         statusCode: 200,
-        body: JSON.stringify(items)
+        body: JSON.stringify(items),
+
+        headers: {
+            // to fix CORS error when trying to access this resource from external clients
+            'Access-Control-Allow-Origin': '*'
+        }
     }
 }
 
