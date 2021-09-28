c95fb4ef93dcc04e96f03057f23075e865274290
Amr Fekry <amrfekryy@gmail.com>
2021-09-28 13:44:47 +0200

    setup netlify redirects

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -2,7 +2,8 @@ const result = document.querySelector('.result')
 
 const fetchData = async () => {
     try {
-        const {data} = await axios.get('/.netlify/functions/1-hello')
+        // const {data} = await axios.get('/.netlify/functions/1-hello')
+        const {data} = await axios.get('/api/1-hello') // redirects is set in netlify.toml
         result.textContent = data
         console.log(data);
     } catch (error) { // error status code will be catched here
